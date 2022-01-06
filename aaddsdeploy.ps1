a920bdac5878f818bf77e8f53dcd9e7dcbf2e8af
Akhil Thomas <akhilthomas011@gmail.com>
2022-01-06 18:09:00 +0530

    Update aaddsdeploy.ps1

--- aaddsdeploy.ps1
+++ aaddsdeploy.ps1
@@ -9,6 +9,8 @@ $VnetName = "aadds-vnet"
 $userName = "wvddomainadmin@$domainName"
 $password = "D0m@!nAdm!n2021"
 
+Install-Module AzureAD -Force
+
 $PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
 $PasswordProfile.Password = $password
 New-AzureADUser -DisplayName "WVD Domain Admin" -PasswordProfile $PasswordProfile -UserPrincipalName $userName -AccountEnabled $true -MailNickName "WVDDomainAdmin"
@@ -30,46 +32,7 @@ if(!($AADDSGroup = Get-AzADGroup -DisplayName "AAD DC Administrators"))
 # Add the user to the 'AAD DC Administrators' group. 
 Add-AzADGroupMember -MemberUserPrincipalName $userName -TargetGroupObjectId $($AADDSGroup).Id -ea SilentlyContinue  
 
-$Params = @{ 
-
-     "domainName" = $domainName 
-
-} 
-
 New-AzResourceGroup -Name $resourceGroupName -Location $location -Force
 
- 
 #Deploy the AADDS template 
 New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateSourceLocation -TemplateParameterObject $Params #Deploy the template #Update Virtual Network DNS servers 
-
-##################### in separate script
-Param (
-    [string]
-    $domainName 
-)
-
-$resourceGroupName = "AVD-RG"
-$location = "eastus"
-$VnetName = "aadds-vnet"
-$userName = "wvddomainadmin@$domainName"
-$password = "D0m@!nAdm!n2021"
-
-Install-Module AzureAD -Force
-
-#Update Virtual Network DNS servers 
-$Vnet = Get-AzVirtualNetwork -Name $VnetName -ResourceGroupName $resourceGroupName 
-$Vnet.DhcpOptions.DnsServers = @() 
-$NICs = Get-AzResource -ResourceGroupName $resourceGroupName -ResourceType "Microsoft.Network/networkInterfaces" -Name "aadds*" 
-ForEach($NIC in $NICs){ 
-$Nicip = (Get-AzNetworkInterface -Name $($NIC.Name) -ResourceGroupName $resourceGroupName).IpConfigurations[0].PrivateIpAddress
- ($Vnet.DhcpOptions.DnsServers).Add($Nicip) 
-
-} 
-
-$Vnet | Set-AzVirtualNetwork    
-
-#Reset user password  
-$securePassword = $password | ConvertTo-SecureString -AsPlainText -Force
-$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $userName, $SecurePassword
-Connect-AzureAD -Credential $cred | Out-Null
-Set-AzureADUserPassword -ObjectId  (Get-AzADUser -UserPrincipalName $userName).Id -Password $securePassword
